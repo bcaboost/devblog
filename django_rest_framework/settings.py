c017fb8523e971e9c09fdaebc58e47f340cee236
Arjun Singh <admin@arjunsingh.com.np>
2020-09-30 17:45:26 +0545

    Parser Setting Update

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
     ]
 }
 
